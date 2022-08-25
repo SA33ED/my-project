6cc206ffc40b77da6d44818e1bb7667393116560
SA33ED <mohammedsaeed892002@gmail.com>
2022-08-25 20:50:44 +0200

    my

--- basic.php
+++ basic.php
@@ -4,7 +4,7 @@ $project_logo="img/logo.png";
 $conn=mysqli_connect('localhost','root','','fof'); //databaseName
 include "functions/all.php";
 session_start();
-echo $_SERVER["REQUEST_URI"];
+
 $x="/my-project/";
 $y="/my-project/main/sign-up.php";
 if($_SERVER["REQUEST_URI"] != $x && $_SERVER["REQUEST_URI"] != $y){
